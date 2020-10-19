62142a8e880abe2132e72f4fb0dc6d74812b4829
curtwphillips <curtwphillips@gmail.com>
2020-10-19 17:16:22 -0400

    add getFunctionBody function

--- adx/functions.js
+++ adx/functions.js
@@ -4,3 +4,12 @@ exports.dropFunctionByName = async (name) => {
   const message = `.drop function ['${name}'] ifexists`;
   await execute(message);
 };
+
+// returns function body as a string
+// ex: "{['test'] | extend col5 = strcat(col4, '_calc'), col6 = strcat(col4, '_calc2')}"
+exports.getFunctionBody = async (functionName) => {
+  const message = `.show function ['${functionName}']`;
+  const response = await execute(message);
+  const rows = standardizeTableResponse(response);
+  return rows && rows[0] && rows[0].Body;
+};
