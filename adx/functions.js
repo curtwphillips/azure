630eea28755d2a2bad1b968ec4ae379a64bf8dab
curtwphillips <curtwphillips@gmail.com>
2020-10-21 13:29:54 -0400

    add regex to remove whitespace, periods, and dashes

--- adx/functions.js
+++ adx/functions.js
@@ -13,3 +13,9 @@ exports.getFunctionBody = async (functionName) => {
   const rows = standardizeTableResponse(response);
   return rows && rows[0] && rows[0].Body;
 };
+
+// removes whitespace, periods, and dashes
+exports.stringToVariableName = (str) => {
+  if (!str) throw new Error('Failed to determine variable name. Input not provided.');
+  return str.replace(/[\s\.-]/gi, '');
+};
